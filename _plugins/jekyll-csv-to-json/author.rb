047c4c9b52aee458cf07c7343a85c1735bafedb4
Jubayed Hossain <70032786+hmjubayed@users.noreply.github.com>
2021-04-21 00:22:09 +0600

    update author

--- _plugins/jekyll-csv-to-json/author.rb
+++ _plugins/jekyll-csv-to-json/author.rb
@@ -1,23 +1,20 @@
 
 
- module CsvToJson
-
+module CsvToJson
     class Generator < Jekyll::Generator
 
         def generate_author(site, movement_files)
 
             data = Hash.new
             movement_files.each do |entry|
-            path = File.join(site.source, @csv_dir, entry)
-            next if File.symlink?(path) && site.safe
-
-            #csv file data csv
-            file_data = CSV.read(path, :headers => true)
+                file_data = read_csv(site, entry)
                 #author_values
-                data[ file_data.headers[1].gsub(' ', '') ] = entry.gsub('movements/', '').gsub(' - MAIN.csv', '')
+                key = file_data.headers[1].gsub(' ', '')
+                value = entry.gsub('movements/', '').gsub(' - MAIN.csv', '')
+                
+                data[key] = value
             end
-
-            save(site, "author", pretty_print(data.sort.to_h.to_json))            
+            save(site, "author", pretty_print(data.sort.to_h.to_json))   
         end
     end
 end
